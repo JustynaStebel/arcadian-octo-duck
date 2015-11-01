5219254d05a74ccf25f7a8fcf667575cdeb20275
JustynaStebel <justyna.stebel@gmail.com>
2015-11-01 21:47:17 +0100

    login with username pt1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
   has_many :products
 
+  validates_presence_of :username
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(where(conditions).where(["username = :value OR lower(email) = lower(:value)", { value: login }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
 end
