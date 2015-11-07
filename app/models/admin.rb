bc44afcb137a5e3e1c3f36981dc095d6ff7766e5
JustynaStebel <justyna.stebel@gmail.com>
2015-11-07 21:47:19 +0100

    Fixes for logging in and out

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,17 +4,4 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.email
-  end
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(email) = :value", { :value => login.to_s.strip.downcase }]).first
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :username
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable, authentication_keys: [:username]
   has_many :products
@@ -13,12 +12,5 @@ class User < ActiveRecord::Base
 
   def email_changed?
     false
-  end
-
-protected
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value", { :value => login.downcase }]).first
   end
 end
